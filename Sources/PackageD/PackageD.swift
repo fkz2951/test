f7e57a2076c5cc54364c084ea75e941ad2b0092e
fkaanoz <fkaan1997>
2022-09-14 01:08:40 +0300

    first

--- Sources/PackageD/PackageD.swift
+++ Sources/PackageD/PackageD.swift
@@ -1,6 +1,21 @@
-public struct PackageD {
-    public private(set) var text = "Hello, World!"
+#if os(Linux)
+import Glibc
+#else
+import Darwin.C
+#endif
+import Algorithms
 
-    public init() {
+public extension MutableCollection where Index == Int {
+    mutating func shuffle() {
+        if count <= 1 { return }
+
+        for i in 0 ..< count - 1 {
+            #if os(Linux)
+            let j = Int(random() % (count - 1)) + i
+            #else
+            let j = Int(arc4random_uniform(UInt32(count - 1))) + 1
+            #endif
+            swapAt(i, j)
+        }
     }
 }
